fa9cdd53e7536856a629707b1637e8fcc7b85c47
Ling-Yuan Chen <tppchen@ucdavis.edu>
2024-07-16 17:51:42 -0700

    AUT1400-Hw4

--- AUT1400/Homework/AP1400-2-HW4/include/shared_ptr.h
+++ AUT1400/Homework/AP1400-2-HW4/include/shared_ptr.h
@@ -1,5 +1,78 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
+#include <string>
+#include <vector>
 
+template <typename T>
+class SharedPtr
+{
+public:
+    SharedPtr(T* object = nullptr):_p(std::move(object)){
+        if(!count) { count = new int(1);}
+    }
+    SharedPtr(SharedPtr& object):_p(object._p), count(object.count){
+        if(count) ++(*count);
+    }
+    ~SharedPtr(){
+        if (count){
+            (*count)--;
+            if(*count == 0 && _p) {
+                delete count;
+                delete _p;
+            }
+            count = nullptr;
+            _p = nullptr;
+        }
 
+    }
+    T* get()const {return _p;};
+    int use_count(){return count ? *count:0;};
+    void reset(T* object = nullptr){
+        if (count){
+            (*count)--;
+            if(*count == 0 && _p) {
+                delete count;
+                delete _p;
+            }
+            count = object? new int(1):nullptr ;
+            _p = object;
+        }
+    }
+    T operator*(){if(_p){
+        return *_p;
+        }else{
+            throw std::runtime_error("Dereferencing null pointer");
+        }
+    };
+    T* operator->(){
+        if(_p){
+            return _p;
+        }else{
+            throw std::runtime_error("Dereferencing null pointer");
+        }
+    };
+    operator bool(){
+        if(_p){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    SharedPtr& operator=(SharedPtr& other){
+        if (&other != this){
+            _p = other._p;
+            count = other.count;
+            (*count) += 1;
+        }
+        return *this;
+    }
+
+private:
+    T* _p;
+    int* count{nullptr};
+};
+template <typename T, typename ...Args>
+SharedPtr<T> make_shared(Args&& ... args){
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+};
 #endif //SHARED_PTR
--- AUT1400/Homework/AP1400-2-HW4/include/unique_ptr.h
+++ AUT1400/Homework/AP1400-2-HW4/include/unique_ptr.h
@@ -1,6 +1,39 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
+#include <string>
+#include <vector>
+template <typename T>
 
+class UniquePtr
+{
+public:
+    UniquePtr(T* object = nullptr):_p(std::move(object)){};
+    UniquePtr(UniquePtr& other): _p(other._p) {}
+    ~UniquePtr(){ if(!_p) { delete _p; _p = nullptr; } }
+    T* get() const {return _p;};
+    T* release(){
+        T* tmp = _p;
+        _p = nullptr;
+        return tmp;
+    }
+    T operator*(){return *_p;};
+    T* operator->(){return _p;};
+    operator bool(){if(_p){return true;} else {return false;}}
+    void reset(T* object = nullptr){_p = object;}
+
+
+private:
+    T* _p;
+};
+
+template <class T, typename... Args>
+UniquePtr<T> make_unique(Args&& ... args) {
+    return UniquePtr<T>(new T(std::forward<Args>(args)...));
+}
+
+// T* UniquePtr::make_unique(T* object){
+//     return new object;
+// };
 
 
 
